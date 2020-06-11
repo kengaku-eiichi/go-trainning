f82598a838ed3b6dab8f91585beb80e4dc473024
kengaku <keng9358@gmail.com>
2020-06-11 19:50:37 +0900

    エラーレポートフォーマット修正

--- zoo/animals/animals_test.go
+++ zoo/animals/animals_test.go
@@ -6,20 +6,20 @@ func TestElephantFeed(t *testing.T) {
 	expect := "Grass"
 
 	if result := ElephantFeed(); expect != result {
-		t.Errorf("expect:%v\n but result:%v\n", expect, result)
+		t.Errorf("\nexpect is  : %v\nbut result : %v\n", expect, result)
 	}
 }
 func TestMonkeyFeed(t *testing.T) {
 	expect := "Banana"
 
 	if result := MonkeyFeed(); expect != result {
-		t.Errorf("expect:%v\n but result:%v\n", expect, result)
+		t.Errorf("\nexpect is  : %v\nbut result : %v\n", expect, result)
 	}
 }
 func TestRabbitFeed(t *testing.T) {
 	expect := "Carrot"
 
 	if result := RabbitFeed(); expect != result {
-		t.Errorf("expect:%v\n but result:%v\n", expect, result)
+		t.Errorf("\nexpect is  : %v\nbut result : %v\n", expect, result)
 	}
 }
