3011402a319ce3f3e32382c7cccdafb31b3a2602
kengaku <keng9358@gmail.com>
2020-06-11 19:23:29 +0900

    package 使用例

---
+++ zoo/main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/kengaku-eiichi/go-trainning/zoo/animals"
+)
+
+func main() {
+	fmt.Println(animals.ElephantFeed())
+	fmt.Println(animals.MonkeyFeed())
+	fmt.Println(animals.RabbitFeed())
+}
